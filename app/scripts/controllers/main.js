8f391ede7653cdb0bb4b097b3a58c7b07df5ad63
erezgiga <erez@gigaspaces.com>
2013-07-22 12:17:37 +0300

    AngularJS implementation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,34 @@
 'use strict';
 
 angular.module('cloudifyWidgetSkinsApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $compile) {
+    $scope.template;
+    $scope.fields;
+    $scope.skinConfiguration = [];
+
+    $scope.$watch('template', function() {
+        var tmpl = $compile($scope.template)($scope);
+        console.log(tmpl);
+        $('#templateContainer').html(tmpl);
+    });
+
+    $scope.$watch('fields', function() {
+        if ($scope.fields !== undefined) {
+            for (var i = 0; i < $scope.fields.length; i++) {
+                $scope.skinConfiguration[$scope.fields[i]] = undefined;
+            }
+        }
+    });
+
   });
+
+function saveUserData() {
+    var body = document.getElementsByTagName('body')[0];
+    var scope = angular.element(body).scope();
+    scope.$apply(function(){
+        if ($('#template').val() != undefined && $('#fieldNames').val() != undefined) {
+            scope.template = $('#template').val();
+            scope.fields = $('#fieldNames').val().split(',');
+        }
+    });
+}
--- app/scripts/directives/getHtml.js
+++ app/scripts/directives/getHtml.js
@@ -3,7 +3,7 @@
 angular.module('cloudifyWidgetSkinsApp')
   .directive('getHtml', function () {
     return {
-      template: '<textarea rows="10" cols="40" readonly/>',
+      template: '<textarea rows="10" cols="40" readonly></textarea>',
       replace:true,
       restrict: 'A',
         scope:{
@@ -11,8 +11,7 @@ angular.module('cloudifyWidgetSkinsApp')
             "valueHolder":"="
         },
       link: function postLink(scope, element, attrs) {
-          scope.$watch('valueHolder',function(){
-              console.log("I have a new value!");
+          scope.$watch('valueHolder', function() {
               element.val($("#" + scope.targetId)[0].outerHTML);
           },true);
       }
