115ec54756ec0aa7dac28f48a4a9a9ba25c44bef
erezgiga <erez@gigaspaces.com>
2013-07-29 15:07:32 +0300

    Skin portal development

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,10 +5,21 @@ angular.module('cloudifyWidgetSkinsApp')
         $scope.fields = "";
 
         var selectedSkin = window.location.hash.split('/').pop();
-        if (selectedSkin != "Clean") {
-            var skin = skinService.getSkin(selectedSkin);
-            $scope.template = skin.template;
-            $scope.fieldNames = JSON.stringify(skin.fields);
-            $scope.backgroundColor = selectedSkin;
+        var skin = skinService.getSkin(selectedSkin);
+
+        $scope.template = skin.template;
+        $scope.fieldNames = skin.fields;
+        $scope.backgroundColor = skin.backgroundColor;
+
+        $scope.saveTemplate = function() {
+            var skin = {
+                "name": "MySkin" + new Date().getMilliseconds(),
+                "description": "My Skin description",
+                "img": "http://www.cloudave.com/wordpress/wp-content/uploads/2011/09/gigaspaces.jpg",
+                "backgroundColor": $("#twitterWidget").css("background-color"),
+                "template": $scope.template,
+                "fields": $scope.fieldNames
+            };
+            skinService.saveSkin(skin);
         }
   });
---
+++ app/scripts/services/mockData.js
@@ -0,0 +1,49 @@
+var skinsJSON = {
+    "skins": [
+        {
+            "name": "Green",
+            "img": "http://www.aiche.org/sites/default/files/styles/aiche_content/public/images/webinar/global_sustainability-green-green-green.jpg",
+            "description": "Green skin",
+            "backgroundColor": "green",
+            "template": "<div><div id='twitterWidget' style='background-color:{{backgroundColor}}'><iframe id='preview' ng-src='/views/skins/twitter.html?widgetId={{widgetId}}&twitterAccount={{twitterAccount.name}}' scrolling='no'></iframe><div id='generatedCode' ng-show='!!showCode'><input type='button' id='closeBtn' value='X' ng-click='showCode=false'/><div get-html target-id='preview' value-holder='widgetId'></div></div></div></div>",
+            "fields": '{"fields": [{"control": "select","name": "twitterAccount","label": "Twitter Account","options": [{"name":"erezcarmel"},{"name":"guymugrabi"}]},{"control": "input","type": "color","name": "backgroundColor","label": "Background Color"},{"control": "input","type": "text","name": "widgetId","label": "Widget ID"}]}'
+        },
+        {
+            "name": "Red",
+            "img": "http://blogs.telegraph.co.uk/culture/files/2011/01/red-light-wallpapers_13156_2560x1920.jpg",
+            "description": "Red skin",
+            "backgroundColor": "red",
+            "template": "<div><div id='twitterWidget' style='background-color:{{backgroundColor}}'><iframe id='preview' ng-src='/views/skins/twitter.html?widgetId={{widgetId}}&twitterAccount={{twitterAccount.name}}' scrolling='no'></iframe><div id='generatedCode' ng-show='!!showCode'><input type='button' id='closeBtn' value='X' ng-click='showCode=false'/><div get-html target-id='preview' value-holder='widgetId'></div></div></div></div>",
+            "fields": '{"fields": [{"control": "select","name": "twitterAccount","label": "Twitter Account","options": [{"name":"erezcarmel"},{"name":"guymugrabi"}]},{"control": "input","type": "color","name": "backgroundColor","label": "Background Color"},{"control": "input","type": "text","name": "widgetId","label": "Widget ID"}]}'
+        },
+        {
+            "name": "Blue",
+            "img": "http://lovely-pics.com/data/media/21/ripple_blue__win_xp_theme.jpg",
+            "description": "Blue skin",
+            "backgroundColor": "blue",
+            "template": "<div><div id='twitterWidget' style='background-color:{{backgroundColor}}'><iframe id='preview' ng-src='/views/skins/twitter.html?widgetId={{widgetId}}&twitterAccount={{twitterAccount.name}}' scrolling='no'></iframe><div id='generatedCode' ng-show='!!showCode'><input type='button' id='closeBtn' value='X' ng-click='showCode=false'/><div get-html target-id='preview' value-holder='widgetId'></div></div></div></div>",
+            "fields": '{"fields": [{"control": "select","name": "twitterAccount","label": "Twitter Account","options": [{"name":"erezcarmel"},{"name":"guymugrabi"}]},{"control": "input","type": "color","name": "backgroundColor","label": "Background Color"},{"control": "input","type": "text","name": "widgetId","label": "Widget ID"}]}'
+        },
+        {
+            "name": "Yellow",
+            "img": "http://www.thewritingnut.com/wp-content/uploads/2011/04/yellow-rose-800.jpg",
+            "description": "Yellow skin",
+            "backgroundColor": "yellow",
+            "template": "<div><div id='twitterWidget' style='background-color:{{backgroundColor}}'><iframe id='preview' ng-src='/views/skins/twitter.html?widgetId={{widgetId}}&twitterAccount={{twitterAccount.name}}' scrolling='no'></iframe><div id='generatedCode' ng-show='!!showCode'><input type='button' id='closeBtn' value='X' ng-click='showCode=false'/><div get-html target-id='preview' value-holder='widgetId'></div></div></div></div>",
+            "fields": '{"fields": [{"control": "select","name": "twitterAccount","label": "Twitter Account","options": [{"name":"erezcarmel"},{"name":"guymugrabi"}]},{"control": "input","type": "color","name": "backgroundColor","label": "Background Color"},{"control": "input","type": "text","name": "widgetId","label": "Widget ID"}]}'
+        }
+    ]
+};
+
+var skinProps = skinsJSON.skins[0];
+
+if(typeof(Storage) !== "undefined")
+{
+    if (localStorage.getItem('skins') == null) {
+        localStorage.setItem('skins', JSON.stringify(skinsJSON));
+    }
+
+    if (localStorage.getItem('skinProps') == null) {
+        localStorage.setItem('skinProps', JSON.stringify(skinProps));
+    }
+}
