2cf264e0263b5a56e0e68bc454073eb6f7b5efc5
erezgiga <erez@gigaspaces.com>
2013-07-31 09:41:48 +0300

    Skin portal development

--- app/scripts/directives/getConfiguration.js
+++ app/scripts/directives/getConfiguration.js
@@ -10,7 +10,7 @@ angular.module('cloudifyWidgetSkinsApp')
                 '<input type="text" id="name" ng-model="name">' +
                 '</div>' +
                 '<div class="userFormField">' +
-                    'Thumbnail' +
+                    'Thumbnail: ' +
                     '<input type="text" id="thumb" ng-model="thumb">' +
                 '</div>' +
                 '<div class="userFormField">' +
@@ -22,10 +22,11 @@ angular.module('cloudifyWidgetSkinsApp')
                     '{control, name, label, [type, options{name}]}' +
                     '<textarea ng-model="fieldNames" id="fieldNames"></textarea>' +
                 '</div>' +
-
-                '<input type="button" value="Save" id="saveButton" class="saveButton" ng-click="updateSkin();"/>' +
-                '<input type="button" value="Save New" id="saveNewButton" class="saveButton" ng-click="addSkin();"/>' +
-                '<input type="button" value="Delete" id="deleteBtn" ng-click="deleteSkin(skin.name);"/>' +
+                '<div id="buttons">' +
+                    '<input type="button" value="Save" id="saveButton" class="saveButton" ng-click="updateSkin();"/>' +
+                    '<input type="button" value="Save New" id="saveNewButton" class="saveButton" ng-click="addSkin();"/>' +
+                    '<input type="button" value="Delete" id="deleteBtn" ng-click="deleteSkin(skin.name);"/>' +
+                '</div>' +
             '</div>',
       restrict: 'A',
       link: function postLink(scope, element, attrs) {
